e0fa7e3389d8742161d4afc25a1fbd0098c33bfc
Ghate <zobeir.m.ghate@accenture.com>
2019-07-31 16:18:55 +0100

    Hello name list

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,16 @@
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Sam!");
+        List<String> teamMembers = new ArrayList<>();
+        teamMembers.add("Atul");
+        teamMembers.add("Sam");
+        teamMembers.add("Zobeir");
+        teamMembers.add("Eliza");
+
+        for(int i=0; i<teamMembers.size();i++) {
+            String name = teamMembers.get(i);
+            System.out.println("Hello " + name);
+        }
+
     }
 }
